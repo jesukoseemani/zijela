53bfc131d8ddf3fec60a427185a00fba4b4a466d
jesukoseemani <olowosusiayo@gmail.com>
2022-05-09 08:44:58 +0100

    initi

--- src/components/Application.js
+++ src/components/Application.js
@@ -1,16 +1,17 @@
-import React from 'react'
+import React from 'react';
 import { Stepper } from 'react-form-stepper';
 import Header from './Header';
 
 function Application() {
-  return (
+	return (
 		<div className='max-w-[1150px] m-auto'>
 			<Stepper
-				steps={[<Header/>, { label: 'Step 2' }, { label: 'Step 3' }]}
+				steps={[<Header />, { label: 'Step 2' }, { label: 'Step 3' }]}
 				activeStep={2}
 			/>
+			<h1>Koseemani olowowsusi</h1>
 		</div>
 	);
 }
 
-export default Application
+export default Application;
